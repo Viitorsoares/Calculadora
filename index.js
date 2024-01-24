690bd95b81ed4a2d333dbfe3e8b236ab2bf4f351
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-24 02:18:23 -0300

    Finalizado projeto calculadora J.S

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+input.addEventListener('keydown', function (ev) {
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+        input.value += ev.key
+        return
+    }
+    if (ev.key === 'Backspace') {
+        input.value = input.value.slice(0, -1)
+    }
+    if (ev.key === 'Enter') {
+        calculate()
+    }
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+function calculate() {
+    resultInput.value = 'ERRO'
+    resultInput.classList.add('error')
+    const result = eval(input.value)
+    resultInput.value = result
+    resultInput.classList.remove('error')
+}
+
+document.querySelectorAll('.charkey').forEach(function (charkeyBtn) {
+    charkeyBtn.addEventListener('click', function () {
+        const value = charkeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+    input.value = ''
+    input.focus()
+})
+
 document.getElementById('themeSwitcher').addEventListener('click', function () {
     if (main.dataset.theme === 'dark') {
         root.style.setProperty('--bg-color', '#f1f5f9')
@@ -21,37 +57,14 @@ document.getElementById('themeSwitcher').addEventListener('click', function () {
     }
 })
 
-document.querySelectorAll('.charkey').forEach(function (charkeyBtn) {
-    charkeyBtn.addEventListener('click', function () {
-        const value = charkeyBtn.dataset.value
-        input.value += value
-    })
-})
-
-document.getElementById('clear').addEventListener('click', function () {
-    input.value = ''
-    input.focus()
-})
-
-document.getElementById('equal').addEventListener('click', calculate)
-
-
-
-function calculate() {
-    const result = eval(input.value)
-    resultInput.value = result
-}
-
-input.addEventListener('keydown', function (ev) {
-    ev.preventDefault()
-    if (allowedKeys.includes(ev.key)) {
-        input.value += ev.key
-        return
-    }
-    if (ev.key === 'Backspace') {
-        input.value = input.value.slice(0, -1)
-    }
-    if (ev.key === 'Enter') {
-        calculate()
+document.getElementById('copyToClipboard').addEventListener('click', function (ev) {
+    const button = ev.currentTarget
+    if (button.innerText === 'Copiar') {
+        button.innerText = 'Copiado!'
+        button.classList.add('sucess')
+        navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = 'Copiar'
+        button.classList.remove('sucess')
     }
 })
