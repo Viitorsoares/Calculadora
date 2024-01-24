becb58564c9b1b231324a96eff30ad2cf21dc056
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-24 01:47:32 -0300

    Atualizei projeto calculadora j.s

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.getElementById('themeSwitcher').addEventListener('click', function () {
+    if (main.dataset.theme === 'dark') {
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26864a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+    }
+})
+
 document.querySelectorAll('.charkey').forEach(function (charkeyBtn) {
     charkeyBtn.addEventListener('click', function () {
         const value = charkeyBtn.dataset.value
@@ -18,6 +34,8 @@ document.getElementById('clear').addEventListener('click', function () {
 })
 
 document.getElementById('equal').addEventListener('click', calculate)
+
+
 
 function calculate() {
     const result = eval(input.value)
