8c70529b3421a9fa6e7c7aa694aa931225ef12b9
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-24 00:48:31 -0300

    Atualizando projeto calculadora

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.querySelectorAll('.charkey').forEach(function (charkeyBtn) {
+    charkeyBtn.addEventListener('click', function () {
+        const value = charkeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+    input.value = ''
+    input.focus()
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+function calculate() {
+    const result = eval(input.value)
+    resultInput.value = result
+}
+
 input.addEventListener('keydown', function (ev) {
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)) {
@@ -18,7 +37,3 @@ input.addEventListener('keydown', function (ev) {
         calculate()
     }
 })
-
-function calculate() {
-    alert('Calculado!')
-}
